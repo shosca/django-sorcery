5af6b15facf3d031341d250fe552e72965bc8c92
Serkan Hosca <serkan@hosca.com>
2018-05-17 12:04:13 -0400

    Add get_object_or_404 shortcut

---
+++ django_sorcery/shortcuts.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from sqlalchemy.exc import InvalidRequestError
+
+from django.http import Http404
+
+from .utils import suppress
+
+
+def _get_query(klass):
+
+    query = getattr(klass, "query", None) or getattr(klass, "objects", None)
+
+    if query:
+        return query
+
+    with suppress(AttributeError, InvalidRequestError):
+        klass._only_entity_zero()
+        return klass
+
+
+def get_object_or_404(klass, *args, **kwargs):
+    """
+    Use session.get() to return an object, or raise a Http404 exception if the object
+    does not exist.
+
+    klass may be a Model, or Query object. All other passed
+    arguments and keyword arguments are used in the get() query.
+    """
+    query = _get_query(klass)
+
+    instance = query.get(*args, **kwargs)
+
+    if instance is None:
+        raise Http404("No %s matches the given query." % query)
+
+    return instance
