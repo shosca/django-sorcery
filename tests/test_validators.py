52ac935d8ef9585e6418fb7ce854ab0b510944a3
Serkan Hosca <serkan@hosca.com>
2018-06-28 10:55:36 -0400

    Add Unique validator (#29)

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -3,9 +3,11 @@ from __future__ import absolute_import, print_function, unicode_literals
 from collections import namedtuple
 
 from django.core.exceptions import ValidationError
-from django.test import TestCase
 
 from django_sorcery.validators import ValidateTogetherModelFields
+
+from .base import TestCase
+from .models import ValidateUniqueModel, db
 
 
 Node = namedtuple("Node", ["left", "right", "value"])
@@ -34,3 +36,28 @@ class TestValidateTogetherModelFields(TestCase):
 
         with self.assertRaises(ValidationError):
             self.assertIsNone(validator(node))
+
+
+class TestValidateUnique(TestCase):
+    def test_validate_on_insert(self):
+        model = ValidateUniqueModel(name="Test")
+        db.add(model)
+        db.flush()
+
+        model = ValidateUniqueModel(name="Test")
+        db.add(model)
+        with self.assertRaises(ValidationError) as ctx:
+            db.flush()
+
+        self.assertEqual(
+            ctx.exception.message_dict,
+            {"__all__": ["name must make a unique set.", "bar, foo must make a unique set."]},
+        )
+
+    def test_validate_on_update(self):
+        model = ValidateUniqueModel(name="Name", foo="Test", bar="Test")
+        db.add(model)
+        db.flush()
+
+        model.foo = "foo"
+        db.flush()
