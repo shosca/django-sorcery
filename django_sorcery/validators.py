8a72650e49f85049eceaa02ae4a6b97435ec0219
Miroslav Shubernetskiy <miroslav@miki725.com>
2018-11-01 18:30:10 -0400

    adding more validators (#60)

--- django_sorcery/validators.py
+++ django_sorcery/validators.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 import sqlalchemy as sa
+from sqlalchemy import inspect
 
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
@@ -81,3 +82,181 @@ class ValidateUnique(object):
 
         if exists:
             raise ValidationError(self.message, code=self.code, params={"fields": ", ".join(sorted(self.attrs))})
+
+
+class ValidateValue(object):
+    """
+    Validator for checking correctness of a value by using a predicate callable
+
+    Useful when other multiple fields need to be consulted
+    to check if particular field is valid.
+
+    For example::
+
+        class MyModel(db.Model):
+            foo = db.Column(db.String())
+            bar = db.Column(db.String())
+
+            validators = [
+                # allow only "bar" value when model.foo == "foo"
+                ValidateValue('bar', lambda m: m.foo != 'foo' or b.bar == 'bar'),
+            ]
+    """
+
+    message = _("Please enter valid value.")
+    code = "invalid"
+
+    negated = False
+
+    def __init__(self, field, predicate, message=None, code=None):
+        assert callable(predicate), "predicate must be callable"
+        self.field = field
+        self.predicate = predicate
+        self.message = message or self.message
+        self.code = code or self.code
+
+    def __call__(self, m):
+        e = ValidationError({self.field: ValidationError(self.message, code=self.code, params={"field": self.field})})
+        try:
+            is_valid = self.predicate(m)
+        except Exception:
+            raise e
+        else:
+            if not is_valid:
+                raise e
+
+
+class ValidateEmptyWhen(object):
+    """
+    Validator for checking a field is empty when predicate is True
+
+    Useful to conditionally enforce a field is not provided depending on related field
+
+    For example::
+
+        class MyModel(db.Model):
+            foo = db.Column(db.String())
+            bar = db.Column(db.String())
+
+            validators = [
+                # do not allow to set bar unless foo is present
+                ValidateEmptyWhen('bar', lambda m: not m.foo),
+            ]
+    """
+
+    message = _("Cannot provide a value to this field.")
+    code = "empty"
+
+    allow_empty = True
+
+    def __init__(self, field, predicate, message=None, code=None):
+        assert callable(predicate), "predicate must be callable"
+        self.field = field
+        self.predicate = predicate
+        self.message = message or self.message
+        self.code = code or self.code
+
+    def __call__(self, m):
+        is_empty = not bool(getattr(m, self.field, None))
+
+        if (self.allow_empty and is_empty) or (not self.allow_empty and not is_empty):
+            return
+
+        if self.predicate(m):
+            raise ValidationError(
+                {self.field: ValidationError(self.message, code=self.code, params={"field": self.field})}
+            )
+
+
+class ValidateNotEmptyWhen(ValidateEmptyWhen):
+    """
+    Validator for checking a field is provided when predicate is True
+
+    Useful to conditionally enforce a field is provided depending on related field
+
+    For example::
+
+        class MyModel(db.Model):
+            foo = db.Column(db.String())
+            bar = db.Column(db.String())
+
+            validators = [
+                # enforce bar is provided when foo is provided
+                ValidateNotEmptyWhen('bar', lambda m: m.foo),
+            ]
+    """
+
+    message = _("This field is required.")
+    code = "not_empty"
+
+    allow_empty = False
+
+
+class ValidateOnlyOneOf(object):
+    """
+    Validate that only one of given fields is provided
+
+    For example::
+
+        class MyModel(db.Model):
+            foo = db.Column(db.String())
+            bar = db.Column(db.String())
+
+            validators = [
+                # enforce only either foo or bar are provided
+                ValidateOnlyOneOf(['foo', 'bar']),
+            ]
+    """
+
+    message = _("Only one of %(fields)s is allowed.")
+    code = "exclusive"
+
+    def __init__(self, fields, required=True, message=None, code=None):
+        self.fields = fields
+        self.required = required
+        self.message = message or self.message
+        self.code = code or self.code
+
+    def __call__(self, m):
+        e = ValidationError(self.message, code=self.code, params={"fields": ", ".join(sorted(self.fields))})
+
+        present = len(list(filter(None, (getattr(m, i, None) for i in self.fields))))
+
+        if (self.required and not present) or present > 1:
+            raise e
+
+
+class ValidateCantRemove(object):
+    """
+   Validate that for data cannot be removed for given field
+
+   For example::
+
+       class MyModel(db.Model):
+           foo = db.Column(db.String())
+
+           validators = [
+               ValidateCantRemove('foo'),
+           ]
+   """
+
+    message = _("Cannot remove existing data.")
+    code = "remove"
+
+    def __init__(self, field, message=None, code=None):
+        self.field = field
+        self.message = message or self.message
+        self.code = code or self.code
+
+    def __call__(self, m):
+        inspected = inspect(m).attrs
+        history = getattr(inspected, self.field).history
+
+        if not history.has_changes():
+            return
+
+        previous, current = next(iter(history.deleted or ()), None), getattr(m, self.field)
+        if previous is not None and current is None:
+            raise ValidationError(
+                {self.field: ValidationError(self.message, code=self.code, params={"field": self.field})}
+            )
--- tests/test_validators.py
+++ tests/test_validators.py
@@ -4,7 +4,14 @@ from collections import namedtuple
 
 from django.core.exceptions import ValidationError
 
-from django_sorcery.validators import ValidateTogetherModelFields
+from django_sorcery.validators import (
+    ValidateCantRemove,
+    ValidateEmptyWhen,
+    ValidateNotEmptyWhen,
+    ValidateOnlyOneOf,
+    ValidateTogetherModelFields,
+    ValidateValue,
+)
 
 from .base import TestCase
 from .testapp.models import ValidateUniqueModel, db
@@ -61,3 +68,109 @@ class TestValidateUnique(TestCase):
 
         model.foo = "foo"
         db.flush()
+
+
+class TestValidateValue(TestCase):
+    def setUp(self):
+        super(TestValidateValue, self).setUp()
+        self.validator = ValidateValue("right", lambda n: n.left != "left" or "right" in n.right)
+
+    def test_valid(self):
+        self.assertIsNone(self.validator(Node("left", "right")))
+        self.assertIsNone(self.validator(Node("notleft", "notRight")))
+        self.assertIsNone(self.validator(Node("notleft", "right")))
+
+    def test_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.validator(Node("left", "left"))
+
+    def test_error(self):
+        with self.assertRaises(ValidationError):
+            self.validator(Node("left", None))
+
+
+class TestValidateEmptyWhen(TestCase):
+    def setUp(self):
+        super(TestValidateEmptyWhen, self).setUp()
+        self.validator = ValidateEmptyWhen("right", lambda n: not n.left)
+
+    def test_valid(self):
+        self.assertIsNone(self.validator(Node(None, None)))
+        self.assertIsNone(self.validator(Node("left", "right")))
+
+    def test_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.validator(Node(None, "right"))
+
+
+class TestValidateNotEmptyWhen(TestCase):
+    def setUp(self):
+        super(TestValidateNotEmptyWhen, self).setUp()
+        self.validator = ValidateNotEmptyWhen("right", lambda n: n.left)
+
+    def test_valid(self):
+        self.assertIsNone(self.validator(Node(None, None)))
+        self.assertIsNone(self.validator(Node("left", "right")))
+
+    def test_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.validator(Node("left", None))
+
+
+class TestValidateOnlyOneOf(TestCase):
+    def test_valid_required(self):
+        validator = ValidateOnlyOneOf(["left", "right"])
+
+        self.assertIsNone(validator(Node(None, "right")))
+        self.assertIsNone(validator(Node("left", None)))
+
+    def test_invalid_required(self):
+        validator = ValidateOnlyOneOf(["left", "right"])
+
+        with self.assertRaises(ValidationError):
+            validator(Node("left", "right"))
+        with self.assertRaises(ValidationError):
+            validator(Node(None, None))
+
+    def test_valid_not_required(self):
+        validator = ValidateOnlyOneOf(["left", "right"], required=False)
+
+        self.assertIsNone(validator(Node(None, "right")))
+        self.assertIsNone(validator(Node("left", None)))
+        self.assertIsNone(validator(Node(None, None)))
+
+    def test_invalid_not_required(self):
+        validator = ValidateOnlyOneOf(["left", "right"], required=False)
+
+        with self.assertRaises(ValidationError):
+            validator(Node("left", "right"))
+
+
+class TestValidateCantRemove(TestCase):
+    def setUp(self):
+        super(TestValidateCantRemove, self).setUp()
+        self.validator = ValidateCantRemove("name")
+
+    def test_no_changes(self):
+        m = ValidateUniqueModel(name="Test")
+        db.add(m)
+        db.flush()
+
+        self.assertIsNone(self.validator(m))
+
+    def test_not_removed(self):
+        m = ValidateUniqueModel(name="Test")
+        db.add(m)
+        db.flush()
+        m.name = "Test2"
+
+        self.assertIsNone(self.validator(m))
+
+    def test_removed(self):
+        m = ValidateUniqueModel(name="Test")
+        db.add(m)
+        db.flush()
+        m.name = None
+
+        with self.assertRaises(ValidationError):
+            self.validator(m)
