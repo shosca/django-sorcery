5489b4c7bfea2ea72c6b231b0b1bf3a5207dc2ef
Miroslav Shubernetskiy <miroslav@miki725.com>
2019-01-08 13:08:45 -0500

    validate only pre-loaded models (#114)
    this avoid validating complete model tree

--- tests/db/test_mixins.py
+++ tests/db/test_mixins.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 from django.core.exceptions import ValidationError
 
 from ..base import TestCase
-from ..testapp.models import Owner, Part, Vehicle
+from ..testapp.models import Owner, Part, Vehicle, VehicleType, db
 
 
 class TestCleanMixin(TestCase):
@@ -20,6 +20,18 @@ class TestCleanMixin(TestCase):
             e.exception.message_dict,
             {"vehicles": [{"paint": ["Can't have a pink car"], "owner": {"first_name": ["Invalid first name"]}}]},
         )
+
+    def test_full_clean_recursive_unloaded(self):
+        owner = Part(vehicles=[Vehicle(type=VehicleType.car, paint="blue", owner=Owner(first_name="valid"))])
+        db.add(owner)
+        db.flush()
+
+        # make owner invalid but pop from instance state
+        # so full_clean() never validates owner
+        owner.vehicles[0].owner.first_name = "invalid"
+        owner.vehicles[0].__dict__.pop("owner")
+
+        owner.full_clean(recursive=True)
 
     def test_full_clean_recursive_empty(self):
         owner = Part(vehicles=[Vehicle(paint="pink", owner=None), None])
